3ac1b286ec5a3b70329b5b44eb1f737ae4385731
CharlesArnal <charles.a.arnal@gmail.com>
2023-04-24 22:08:15 +0200

    plotting improved

--- plot_ROC_binding.py
+++ plot_ROC_binding.py
@@ -41,15 +41,15 @@ def percentage_of_binding_above_thresh(preds, labels, thresh):
                 above_thresh_and_binding += 1
     return (above_thresh_and_binding/above_thresh if above_thresh != 0.0 else 1)
 
-def percentage_of_binding_below_thresh(preds,labels, thresh):
-    below_thresh_and_binding = 0.0
+def percentage_of_non_binding_below_thresh(preds,labels, thresh):
+    below_thresh_and_non_binding = 0.0
     below_thresh = 0.0
     for index, pred in enumerate(preds):
         if pred <thresh:
             below_thresh += 1
-            if labels[index] == 1:
-                below_thresh_and_binding += 1
-    return (below_thresh_and_binding/below_thresh if below_thresh != 0.0 else 0)
+            if labels[index] == 0:
+                below_thresh_and_non_binding += 1
+    return (below_thresh_and_non_binding/below_thresh if below_thresh != 0.0 else 1)
 
 
 
@@ -79,16 +79,30 @@ fpr = [false_positive_rate(preds, labels, thresh ) for thresh in np.linspace(0,1
 # plt.show()
 
 threshs = np.linspace(0,1,100)
-pbat = [percentage_of_binding_above_thresh(preds, labels, thresh ) for thresh in threshs]
-pbbt = [percentage_of_binding_below_thresh(preds, labels, thresh ) for thresh in threshs]
+pbat = [percentage_of_binding_above_thresh(preds, labels, thresh )*100 for thresh in threshs]
+pnbbt = [percentage_of_non_binding_below_thresh(preds, labels, thresh )*100 for thresh in threshs]
 
 
 fig1 = plt.figure()
-plt.plot(threshs, pbat)
+plt.plot(threshs, pbat )
+plt.xlabel("Threshold")
+plt.ylabel("% of binding ZFs above the threshold")
+plt.xticks(ticks = np.linspace(0,1,11))
+plt.yticks(ticks = np.linspace(40,100,13))
+plt.grid(axis="y")
+
 fig2 = plt.figure()
-plt.plot(threshs, pbbt)
+plt.plot(threshs, pnbbt )
+plt.xlabel("Threshold")
+plt.ylabel("% of non-binding ZFs below the threshold")
+plt.xticks(ticks = np.linspace(0,1,11))
+plt.yticks(ticks = np.linspace(40,100,13))
+plt.grid(axis="y")
+
 plt.show()
 
+print(f"Percentage of binding above {0.8} : {percentage_of_binding_above_thresh(preds, labels, 0.8)}")
+print(f"Percentage of  non-binding below {0.8} : {percentage_of_non_binding_below_thresh(preds, labels, 0.8)}")
 
 # proteins = [[] for i in range(num_proteins)]
 # for index, protein_id in enumerate(protein_ids):
